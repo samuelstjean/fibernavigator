9c3b1f32457528a72117521bafb48cba56ff7587
Samuel St-Jean <Samuel.St-Jean@usherbrooke.ca>
2014-08-20 16:06:46 -0400

    stuff

---
+++ src/version/VersionString.cpp
@@ -0,0 +1,5 @@
+#define GIT_SHA1 "d5fe599635cf6127a1d2c2f23b299b525b575938"
+extern const char VERSION_GIT_SHA1[] = GIT_SHA1;
+
+extern const char VERSION_BUILD_DATE[] = __DATE__;
+extern const char VERSION_BUILD_TIME[] = __TIME__;
